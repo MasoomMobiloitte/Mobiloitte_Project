8de58b39a6f7038a7598c1597f5d8e6b05f7b837
Vishal <smartboyvishal30@gmail.com>
2022-03-25 22:53:50 +0530

    adding home fragment

--- app/src/main/java/com/example/mobiloitte_project/MainActivity.kt
+++ app/src/main/java/com/example/mobiloitte_project/MainActivity.kt
@@ -1,15 +1,58 @@
 package com.example.mobiloitte_project
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mobiloitte_project.databinding.ActivityMainBinding
+import com.example.mobiloitte_project.fragment.HomeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.bottomnavigation.LabelVisibilityMode
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding :ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // add title in app
+        setupToolbar()
+
+        // cll default fragment
+        openHome()
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> {
+                    Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame, HomeFragment())
+                        .commit()
+                }
+
+            }
+            return@setOnItemSelectedListener true
+
+        }
+    }
+
+    fun openHome(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame, HomeFragment())
+            .commit()
+        supportActionBar?.title = "Home"
+    }
+
+
+    // added title function
+    fun setupToolbar(){
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = "Home"
     }
 }
