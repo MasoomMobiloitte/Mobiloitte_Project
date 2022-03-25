33dd409f7f3313d5966822b0ad7e79c6f525509a
Masoom Zaid <an-masoom@indicchain.com>
2022-03-25 12:34:26 +0530

    trying nav bar

--- app/src/main/java/com/example/mobiloitte_project/MainActivity.kt
+++ app/src/main/java/com/example/mobiloitte_project/MainActivity.kt
@@ -2,14 +2,14 @@ package com.example.mobiloitte_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomnavigation.LabelVisibilityMode
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
     }
 }
